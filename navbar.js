70c7e04cfa9791fe620cad16437d7a92ce4d9ce8
sunnismystrength <sunn.ismystrength@gmail.com>
2025-08-28 13:24:29 -0400

    Create navbar.js

---
+++ navbar.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("navbar").innerHTML = `
+    <ul>
+      <li><a href="index.html">Home</a></li>
+      <li><a href="auction.html">Auctions</a></li>
+      <li><a href="store.html">Store</a></li>
+      <li><a href="learn.html">Learn Bitcoin</a></li>
+      <li><a href="disclosure.html">Disclosure</a></li>
+      <li><a href="backoffice.html">Back Office</a></li>
+    </ul>
+  `;
+});
